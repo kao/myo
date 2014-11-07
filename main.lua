f8b5a2bcd081a826594a992a2ef318bc6c525ed9
Flavien Cogez <flavien.cogez@gmail.com>
2014-11-07 16:15:00 +0100

    Remote control with Spotify waveIn, waveOut to navigate in the playlist, fingersSprend to play song

---
+++ main.lua
@@ -0,0 +1,76 @@
+scriptId = 'com.kao.spotify.remote'
+
+-- Unlock mechanism
+function unlock()
+  unlocked = true
+  extendUnlock()
+end
+
+function extendUnlock()
+  unlockedSince = myo.getTimeMilliseconds()
+end
+
+-- Callbacks
+function onPoseEdge(pose, edge)
+  -- Unlock
+  myo.debug('pose: ' .. pose .. ' || edge: ' .. edge)
+  myo.debug('----')
+
+  if pose == 'fist' then
+    if edge == 'off' then
+      unlock()
+    elseif edge == 'on' and not unlocked then
+      myo.vibrate('short')
+      myo.vibrate('short')
+      extendUnlock()
+    end
+  end
+
+  if pose == 'waveIn' or pose == 'waveOut' then
+    if unlocked and edge == 'on' then
+      if pose == 'waveIn' then
+        myo.keyboard('f8', 'press')
+      else
+        myo.keyboard('up_arrow', 'press')
+      end
+
+      myo.vibrate('short')
+      extendUnlock()
+    end
+  end
+
+  if pose == 'fingersSpread' then
+    myo.keyboard('return', 'press')
+  end
+
+end
+
+
+function onPeriodic()
+  local foo
+end
+
+function onForegroundWindowChange(app, title)
+  local wantActive = false
+  activeApp = ''
+
+  if app == 'com.spotify.music' then
+    myo.debug('Spotify detected')
+    wantActive = true
+    activeApp = 'Spotify'
+  end
+
+  return wantActive
+end
+
+function activeAppName()
+  -- Return the active app name determined in onForegroundWindowChange
+  myo.debug(activeApp)
+  return activeApp
+end
+
+function onActiveChange(isActive)
+  if not isActive then
+    unlocked = false
+  end
+end
